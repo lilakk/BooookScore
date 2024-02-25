b1fdd700aff1d667e9959f401c936b3c3d2a6925
Ilya Gusev <phoenixilya@gmail.com>
2024-02-25 22:20:05 +0100

    Booookscore v2: batched version

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,35 +1,55 @@
-from transformers import GPT2Tokenizer
+import os
+import traceback
 import time
+
 import openai
+from transformers import GPT2Tokenizer
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-
-openai.api_key = "YOUR_API_KEY"
 
 
 def count_tokens(text):
     return len(tokenizer.encode(text))
 
 
-def get_response(prompt: str, max_tokens = 0, temperature = 0.1, top_p = 1, n = 1, logprobs = 1, stop = None, echo = False):
-    response = openai.ChatCompletion.create(model="gpt-4",  # NOTE: change this as you need
-                                        messages=[
-                                            {"role": "user", "content": prompt}
-                                        ],
-                                        temperature=temperature,
-                                        max_tokens=max_tokens)
+def get_response(
+    prompt: str,
+    model_name: str,
+    max_tokens: int,
+    temperature: int,
+):
+    response = openai.ChatCompletion.create(
+        model=model_name,
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        temperature=temperature,
+        max_tokens=max_tokens
+    )
     return response
 
 
-def obtain_response(prompt: str, max_tokens: int, temperature: float, echo = False):
+def obtain_response(
+    prompt: str,
+    max_tokens: int = 1024,
+    temperature: float = 0.1,
+    model_name: str = "gpt-4-turbo-preview",
+    echo: bool = False
+):
     response = None
     num_attemps = 0
     while response is None:
         try:
-            response = get_response(prompt, max_tokens=max_tokens, temperature=temperature, echo=echo)
+            response = get_response(
+                prompt,
+                max_tokens=max_tokens,
+                temperature=temperature,
+                model_name=model_name
+            )
         except Exception as e:
-            print(e)
+            print(traceback.format_exc())
             num_attemps += 1
             print(f"Attempt {num_attemps} failed, trying again after 5 seconds...")
             time.sleep(5)
-    return response['choices'][0]['message']['content'].strip()
+
+    return response["choices"][0]["message"]["content"].strip()
