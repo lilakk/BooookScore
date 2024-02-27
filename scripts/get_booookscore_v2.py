3f543b714045106eccf0f1db638da498c096ed0e
Ilya Gusev <phoenixilya@gmail.com>
2024-02-27 21:36:52 +0100

    Parallel requests to make it faster

--- scripts/get_booookscore_v2.py
+++ scripts/get_booookscore_v2.py
@@ -4,6 +4,8 @@ import time
 import shutil
 import traceback
 from typing import List, Any, Dict, Optional
+from multiprocessing.pool import ThreadPool
+from threading import Lock
 
 import fire
 import tqdm
@@ -17,13 +19,14 @@ DEFAULT_CACHE_DIR = "gpt4_annotations/v2"
 DEFAULT_MODEL_NAME = "gpt-4-turbo-preview"
 DEFAULT_TEMPLATE_PATH = "prompts/get_gpt4_annotations_v2.txt"
 DEFAULT_BATCH_SIZE = 10
+DEFAULT_PARALLEL_REQUESTS_COUNT = 2
 
 
 def gen_batch(records: List[Any], batch_size: int):
     batch_start = 0
     while batch_start < len(records):
         batch_end = batch_start + batch_size
-        batch = records[batch_start: batch_end]
+        batch = records[batch_start:batch_end]
         batch_start = batch_end
         yield batch
 
@@ -31,8 +34,40 @@ def gen_batch(records: List[Any], batch_size: int):
 def parse_response(response):
     start_index = response.find("[")
     end_index = response.rfind("]") + 1
-    answers = json.loads(response[start_index: end_index])
+    answers = json.loads(response[start_index:end_index])
     return answers
+
+
+def calc_instance(summary, sentences, template, num_retries, model_name):
+    formatted_batch = "\n".join([f"{n+1}. {s}" for n, s in enumerate(sentences)])
+    prompt = template.format(summary=summary, sentences=formatted_batch)
+    for _ in range(num_retries):
+        try:
+            response = obtain_response(prompt, model_name=model_name)
+            answers = parse_response(response)
+            break
+        except Exception:
+            print(traceback.format_exc())
+            time.sleep(10)
+    assert len(answers) == len(sentences)
+    for answer, sentence in zip(answers, sentences):
+        answer["sentence"] = sentence
+    return answers
+
+
+def create_callback(book, cache, cache_path, lock):
+    def on_result(result):
+        with lock:
+            for answer in result:
+                sentence = answer.pop("sentence")
+                cache[book][sentence] = answer
+                print(f"Cache updated: {sentence} -> {answer}")
+            cache_temp_path = cache_path + ".tmp"
+            with open(cache_temp_path, "w") as w:
+                json.dump(cache, w)
+            shutil.move(cache_temp_path, cache_path)
+
+    return on_result
 
 
 def calc_booookscore_v2(
@@ -41,6 +76,7 @@ def calc_booookscore_v2(
     model_name: str = DEFAULT_MODEL_NAME,
     template_path: str = DEFAULT_TEMPLATE_PATH,
     batch_size: int = DEFAULT_BATCH_SIZE,
+    parallel_requests_count: int = DEFAULT_PARALLEL_REQUESTS_COUNT,
     num_retries: int = 3,
 ):
     cache = defaultdict(dict)
@@ -50,35 +86,28 @@ def calc_booookscore_v2(
             cache = defaultdict(dict, cache)
         print(f"LOADED CACHE FROM {cache_path}")
 
-    with open(template_path, 'r') as f:
+    with open(template_path, "r") as f:
         template = f.read()
 
-    for book, summary in tqdm.tqdm(summaries.items(), total=len(summaries), desc="Iterating over summaries"):
+    for book, summary in tqdm.tqdm(
+        summaries.items(), total=len(summaries), desc="Iterating over summaries"
+    ):
         sentences = sent_tokenize(summary)
         sentences = [s for s in sentences if s not in cache[book]]
 
+        lock = Lock()
         batches = list(gen_batch(sentences, batch_size))
-        for batch in tqdm.tqdm(batches, total=len(batches), desc="Iterating over batched sentences"):
-            formatted_batch = "\n".join([f"{n+1}. {s}" for n, s in enumerate(batch)])
-            print(f"\nSENTENCES:\n\n{batch}\n")
-            prompt = template.format(summary=summary, sentences=formatted_batch)
-            for _ in range(num_retries):
-                try:
-                    response = obtain_response(prompt, model_name=model_name)
-                    print(f"RESPONSE:\n\n{response}\n")
-                    answers = parse_response(response)
-                    assert len(answers) == len(batch)
-                    break
-                except Exception:
-                    print(traceback.format_exc())
-                    time.sleep(10)
-            for sentence, answer in zip(batch, answers):
-                cache[book][sentence] = answer
-
-            cache_temp_path = cache_path + ".tmp"
-            with open(cache_temp_path, "w") as w:
-                json.dump(cache, w)
-            shutil.move(cache_temp_path, cache_path)
+        tasks = [
+            (summary, batch, template, num_retries, model_name) for batch in batches
+        ]
+        with ThreadPool(parallel_requests_count) as pool:
+            callback = create_callback(book, cache, cache_path, lock)
+            results = [
+                pool.apply_async(calc_instance, args=task, callback=callback)
+                for task in tasks
+            ]
+            for result in results:
+                result.get()
 
     scores = dict()
     for book, answers in cache.items():
@@ -96,7 +125,8 @@ def get_booookscore_v2(
     cache_path: Optional[str] = None,
     model_name: str = DEFAULT_MODEL_NAME,
     template_path: str = DEFAULT_TEMPLATE_PATH,
-    batch_size: int = DEFAULT_BATCH_SIZE
+    batch_size: int = DEFAULT_BATCH_SIZE,
+    parallel_requests_count: int = DEFAULT_PARALLEL_REQUESTS_COUNT,
 ):
     if cache_path is None:
         cache_path = os.path.join(DEFAULT_CACHE_DIR, os.path.basename(input_path))
@@ -107,7 +137,8 @@ def get_booookscore_v2(
         cache_path=cache_path,
         model_name=model_name,
         template_path=template_path,
-        batch_size=batch_size
+        batch_size=batch_size,
+        parallel_requests_count=parallel_requests_count,
     )
     print(f"Average confusion score: {avg_score}")
 
